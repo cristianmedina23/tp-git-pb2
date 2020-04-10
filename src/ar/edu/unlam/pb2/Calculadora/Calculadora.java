e32e01187a5a1abe24318b9f8ff7204d6aa8358b
cristian <cristianmedinaranda@hotmail.com>
2020-04-10 17:33:12 -0300

    cambio del if-else por un swhich

--- src/ar/edu/unlam/pb2/Calculadora/Calculadora.java
+++ src/ar/edu/unlam/pb2/Calculadora/Calculadora.java
@@ -7,7 +7,7 @@ public class Calculadora {
             Integer a;    
             Integer b;          
             Integer opcion;
-            Integer resultado;
+            Integer resultado = null;
            
             Scanner teclado = new Scanner(System.in);
             do{
@@ -20,19 +20,28 @@ public class Calculadora {
            
             System.out.println("Ingrese el segundo numero");  
             b = teclado.nextInt();
-           
-            if(opcion.equals(1))
-            {
-                resultado = a + b;     
-            }else
-                if(opcion.equals(2))
-                {    resultado = a - b;  
-            }else
-                if(opcion.equals(3))
-                {   resultado = a * b;      
-            }else
-            {     resultado = a / b;         
-            }
+            switch (opcion) {
+            case 1:
+                System.out.println("Has seleccionado la opcion 1");
+                resultado = a+b;
+                break;
+            case 2:
+                System.out.println("Has seleccionado la opcion 2");
+                resultado = a-b;
+                break;
+            case 3:
+                System.out.println("Has seleccionado la opcion 3");
+                resultado = a*b;
+                break;
+            case 4:
+            	   System.out.println("Has seleccionado la opcion 4");
+            	   resultado = a/b;
+                break;
+            default:
+                System.out.println("Solo números entre 1 y 4");
+        }
+                 
+            
             System.out.println("El resultado es " + resultado.toString());
             }
         }
