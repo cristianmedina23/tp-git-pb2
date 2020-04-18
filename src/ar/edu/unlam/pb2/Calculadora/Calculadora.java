d912528c3e64acbdad42f3cf6fa6e2ea72ee5523
atomasini <atomasini@vantek.co>
2020-04-18 02:26:43 -0300

    Grupo2| arreglo de validacion

--- src/ar/edu/unlam/pb2/Calculadora/Calculadora.java
+++ src/ar/edu/unlam/pb2/Calculadora/Calculadora.java
@@ -8,15 +8,15 @@ public class Calculadora {
 		Integer a;
 		Integer b;
 		Integer opcion;
-            Integer resultado = null;
+        Integer resultado = null;
 
 		Scanner teclado = new Scanner(System.in);
 		do {
 			System.out.println(
 					"Ingrese su operación \n 1 para multiplicar \n 2 para restar \n 3 para sumar \n 4 para dividir");
 			opcion = teclado.nextInt();
-            }
-            while (opcion < 1 && opcion > 4);
+            }                                   
+            while (opcion < 1 || opcion > 4);
 		System.out.println("Ingrese el primer numero");
 		a = teclado.nextInt();
 
